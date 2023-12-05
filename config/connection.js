f02c93dd43c8c0a87fedfa07f27031cded8595d3
Hector <141383937+hfelix3@users.noreply.github.com>
2023-12-04 20:22:01 -0700

    added database models

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,4 @@
+// this is requiring the .env package for .env file where we hold the variables for our secrets
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
@@ -7,7 +8,9 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: '127.0.0.1',
       dialect: 'mysql',
+      // TODO: add [socketPath: 'tmp/mysql.sock',] for mac users
       dialectOptions: {
+        socketPath: 'tmp/mysql.sock',
         decimalNumbers: true,
       },
     });
--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+//TODO: import sequelize connection
+const sequelize = require ('../config/connection');
 
+// TODO: import models
+const category = 
+
+// initialize application
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+// initialize connection
+// TODO: add sequelize.sync().then(() => {
 // sync sequelize models to the database, then turn on the server
+sequelize.sync().then(() => {
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+})
+
 });
